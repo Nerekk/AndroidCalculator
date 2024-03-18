578a1b16a00a5c61b3c43f72643984e3438e5a4e
Nerekk <arkadiusz.salaj.02@gmail.com>
2024-03-18 16:54:56 +0100

    Changing main activity

---
+++ app/src/main/java/com/example/apptest/MenuActivity.java
@@ -0,0 +1,23 @@
+package com.example.apptest;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MenuActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_menu);
+    }
+
+    public void goToBasic(View view) {
+        Intent intent = new Intent(this, BasicActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
