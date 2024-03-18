6e1827f83cb9121f40c7cc55c91381e6c81436a5
Nerekk <arkadiusz.salaj.02@gmail.com>
2024-03-18 16:57:28 +0100

    Info activity implementation

---
+++ app/src/main/java/com/example/apptest/InfoActivity.java
@@ -0,0 +1,17 @@
+package com.example.apptest;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class InfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_info);
+
+    }
+}
